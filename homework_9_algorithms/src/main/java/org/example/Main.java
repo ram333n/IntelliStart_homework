4fe1bae958f0ff8fe6024123480b339cc463ed5f
Roman Prokopchuk <71777440+ram333n@users.noreply.github.com>
2022-09-10 21:53:39 +0300

    HW9: added quick sort implementation

--- homework_9_algorithms/src/main/java/org/example/Main.java
+++ homework_9_algorithms/src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import org.example.sort.QuickSorter;
+import org.example.sort.Sorter;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Integer[] array = {-1,2,-3,4,10,-4242};
+        Sorter<Integer> sorter = new QuickSorter<>();
+        sorter.sort(array, Comparator.reverseOrder());
+        System.out.println(Arrays.toString(array));
     }
 }
